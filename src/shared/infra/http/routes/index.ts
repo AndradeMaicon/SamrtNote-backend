622047d40aec2f6188a59a67dd82837ac8ff7a15
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-11-02 12:36:19 -0300

    User route an controller created

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import tasksRoute from '@modules/tasks/infra/http/routes/task.routes';
+import userRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/tasks', tasksRoute);
+routes.use('/users', userRouter);
 
 export default routes;
