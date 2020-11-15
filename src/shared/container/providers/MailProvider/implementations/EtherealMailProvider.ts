3201d02be236de70bc12e876e31fb3c84c5befa9
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-11-15 15:14:40 -0300

    EtherealMailProvider changed

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -34,8 +34,6 @@ export default class EtherealMailProvider implements IMailProvider {
     subject,
     templateData,
   }: ISendMailDTO): Promise<void> {
-    console.log(to, from, subject, templateData);
-
     const message = await this.client.sendMail({
       from: {
         name: from?.name || 'Equipe SmartNote',
